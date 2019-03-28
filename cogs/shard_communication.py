702c2239a3b15554fc84edc46e745c672c71fed1
Diniboy1123 <Diniboy1123@users.noreply.github.com>
2019-03-28 22:59:19 +0000

    Missing header addition

--- cogs/shard_communication.py
+++ cogs/shard_communication.py
@@ -1,3 +1,10 @@
+"""
+The IdleRPG Discord Bot
+Copyright (C) 2018-2019 Diniboy and Gelbpunkt
+This software is dual-licensed under the GNU Affero General Public License for non-commercial and the Travitia License for commercial use.
+For more information, see README.md and LICENSE.md.
+"""
+
 from traceback import format_exc
 import asyncio
 
