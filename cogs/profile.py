359b17d0f488846fc702f27b511412cc96101af0
Max <47576240+maxx0911@users.noreply.github.com>
2019-03-31 15:19:59 +0200

    now with `display_name`
    not too sure what passing ctx means ~~(don't be mad at me pls)~~, but I put `ctx` in line 217 and 251, if that's what you meant.

--- cogs/profile.py
+++ cogs/profile.py
@@ -216,7 +216,7 @@ class Profile(commands.Cog):
 
     async def invembed(self, ctx, ret):
         result = discord.Embed(
-            title=f"{ctx.author.nick}'s inventory includes", colour=discord.Colour.blurple()
+            title=f"{ctx.author.display_name}'s inventory includes", colour=discord.Colour.blurple()
         )
         for weapon in ret:
             if weapon[7]:
@@ -248,7 +248,7 @@ class Profile(commands.Cog):
             allitems = list(chunks(ret, 5))
             currentpage = 0
             maxpage = len(allitems) - 1
-            result = await self.invembed(allitems[currentpage])
+            result = await self.invembed(ctx, allitems[currentpage])
             result.set_footer(text=f"Page {currentpage+1} of {maxpage+1}")
             msg = await ctx.send(embed=result)
             if maxpage == 0:
