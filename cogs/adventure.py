b577e671620bd145a090c68c5ed51e5d369506d1
Jens Reidel <jens@troet.org>
2019-04-13 11:42:34 +0200

    This should be a fair calculation

--- cogs/adventure.py
+++ cogs/adventure.py
@@ -644,7 +644,7 @@ Use attack, defend or recover
                             guild,
                         )
                     # !!! TEMPORARY EASTER EVENT !!!
-                    eggs = int(round(isfinished[3] ** 1.2 * random.randint(1, 4), 0))
+                    eggs = int(round(isfinished[3] ** 1.5 * random.randint(4, 6), 0))
                     await conn.execute(
                         'UPDATE profile SET "money"="money"+$1, "xp"="xp"+$2, "completed"="completed"+1, "eastereggs"="eastereggs"+$3 WHERE "user"=$4;',
                         gold,
