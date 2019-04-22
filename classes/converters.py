b9c40f7cd2913a271d68e40ffb3baffd0876ee75
Gelbpununkt <jens@troet.org>
2019-04-22 12:09:56 +0000

    added IntFromTo(start, end)

--- classes/converters.py
+++ classes/converters.py
@@ -11,6 +11,13 @@ import discord
 from discord.ext import commands
 
 
+class NotInRange(commands.BadArgument):
+    def __init__(self, text, from_, to_):
+        self.text = text
+        self.from_ = from_
+        self.to_ = to_
+
+
 class User(commands.Converter):
     async def convert(self, ctx, argument):
         # TODO: Try the local users first
@@ -24,3 +31,22 @@ class User(commands.Converter):
         user = discord.User(state=ctx.bot._connection, data=data)
         ctx.bot.users.append(user)
         return user
+
+
+class IntFromTo(commands.Converter):
+    def __init__(self, from_, to_):
+        self.from_ = from_
+        self.to_ = to_
+
+    async def convert(self, ctx, arg):
+        try:
+            arg = int(arg)
+        except ValueError:
+            raise commands.BadArgument("Converting to int failed.")
+        if not self.from_ <= arg <= self.to_:
+            raise NotInRange(
+                f"The supplied number must be in range of {self.from_} to {self.to_}.",
+                self.from_,
+                self.to_,
+            )
+        return arg
