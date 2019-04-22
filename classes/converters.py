ad05bc0bbca1464ff1026645ccf017a9e8ebd5ad
Jens Reidel <jens@troet.org>
2019-04-22 16:53:45 +0200

    Added UserWithCharacter and MemberWithCharacter
    Needs black

--- classes/converters.py
+++ classes/converters.py
@@ -6,6 +6,7 @@ License for non-commercial and the Travitia License for commercial
 use.
 For more information, see README.md and LICENSE.md.
 """
+import re
 
 import discord
 from discord.ext import commands
@@ -17,6 +18,8 @@ class NotInRange(commands.BadArgument):
         self.from_ = from_
         self.to_ = to_
 
+class UserHasNoChar(commands.BadArgument):
+    pass
 
 class User(commands.Converter):
     async def convert(self, ctx, argument):
@@ -32,6 +35,49 @@ class User(commands.Converter):
         ctx.bot.users.append(user)
         return user
 
+class UserWithCharacter(commands.Converter):
+    async def convert(self, ctx, argument):
+        # TODO: Try the local users first
+        data = await ctx.bot.cogs["Sharding"].handler(
+            "fetch_user", 1, {"user_inp": argument}
+        )
+        if not data:
+            raise commands.BadArgument("Unknown user.", argument)
+        data = data[0]
+        data["username"] = data["name"]
+        user = discord.User(state=ctx.bot._connection, data=data)
+        ctx.bot.users.append(user)
+        user.character_data = await ctx.bot.pool.fetchrow('SELECT * FROM profile WHERE "user"=$1;', user.id)
+        if user.character_data:
+            return user
+        else:
+            raise UserHasNoChar("User has no character.", user)
+
+class MemberWithCharacter(commands.converter.IDConverter):
+    async def convert(self, ctx, argument):
+        match = self._get_id_match(argument) or re.match(r'<@!?([0-9]+)>$', argument)
+        result = None
+        if match is None:
+            # not a mention...
+            if ctx.guild:
+                result = ctx.guild.get_member_named(argument)
+            else:
+                result = commands.converter._get_from_guilds(ctx.bot, 'get_member_named', argument)
+        else:
+            user_id = int(match.group(1))
+            if guild:
+                result = ctx.guild.get_member(user_id)
+            else:
+                result = commands.converter._get_from_guilds(ctx.bot, 'get_member', user_id)
+
+        if result is None:
+            raise BadArgument(f"Member "{argument}" not found")
+
+        result.character_data = await ctx.bot.pool.fetchrow('SELECT * FROM profile WHERE "user"=$1;', result.id)
+        if result.character_data:
+            return result
+        else:
+            raise UserHasNoChar("User has no character.", result)
 
 class IntFromTo(commands.Converter):
     def __init__(self, from_, to_):
