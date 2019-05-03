07cc06f92b97d4d94fd74d1039d932bc5a87d337
Jens Reidel <jens@troet.org>
2019-05-03 13:15:17 +0200

    Refactored ranks.py

--- cogs/ranks.py
+++ cogs/ranks.py
@@ -5,8 +5,6 @@ Copyright (C) 2018-2019 Diniboy and Gelbpunkt
 This software is dual-licensed under the GNU Affero General Public License for non-commercial and the Travitia License for commercial use.
 For more information, see README.md and LICENSE.md.
 """
-
-
 import discord
 from discord.ext import commands
 
@@ -17,95 +15,66 @@ class Ranks(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(description="Shows the richest players. Maximum 10.")
+    @commands.command()
     async def richest(self, ctx):
+        """The 10 richest players."""
         await ctx.trigger_typing()
-        async with self.bot.pool.acquire() as conn:
-            ret = await conn.fetch(
-                'SELECT "user", "name", "money" FROM profile ORDER BY "money" DESC LIMIT 10;'
-            )
-        if ret == []:
-            return await ctx.send(
-                "No character have been created yet. Use `{ctx.prefix}create` to be the first one!"
-            )
+        players = await self.bot.pool.fetch(
+            'SELECT "user", "name", "money" FROM profile ORDER BY "money" DESC LIMIT 10;'
+        )
         result = ""
-        for profile in ret:
-            number = ret.index(profile) + 1
-            charname = await rpgtools.lookup(self.bot, profile[0])
-            pstring = f"{number}. {profile[1]}, a character by `{charname}` with **${profile[2]}**\n"
-            result += pstring
+        for idx, profile in enumerate(players):
+            username = await rpgtools.lookup(self.bot, profile["user"])
+            result = f"{result}{idx + 1}. {profile['name']}, a character by `{username}` with **${profile['money']}**\n"
         result = discord.Embed(
             title="The Richest Players", description=result, colour=0xE7CA01
         )
         await ctx.send(embed=result)
 
-    @commands.command(
-        aliases=["best", "high", "top"],
-        description="Shows the best players sorted by XP. Maximum 10.",
-    )
+    @commands.command(aliases=["best", "high", "top"])
     async def highscore(self, ctx):
+        """The top 10 players by XP."""
         await ctx.trigger_typing()
-        async with self.bot.pool.acquire() as conn:
-            ret = await conn.fetch(
-                'SELECT "user", "name", "xp" from profile ORDER BY "xp" DESC LIMIT 10;'
-            )
-        if ret == []:
-            return await ctx.send(
-                "No character have been created yet. Use `{ctx.prefix}create` to be the first one!"
-            )
+        players = await self.bot.pool.fetch(
+            'SELECT "user", "name", "xp" from profile ORDER BY "xp" DESC LIMIT 10;'
+        )
         result = ""
-        for profile in ret:
-            number = ret.index(profile) + 1
-            charname = await rpgtools.lookup(self.bot, profile[0])
-            pstring = f"{number}. {profile[1]}, a character by `{charname}` with Level **{rpgtools.xptolevel(profile[2])}** (**{profile[2]}** XP)\n"
-            result += pstring
+        for idx, profile in enumerate(players):
+            username = await rpgtools.lookup(self.bot, profile["user"])
+            result = f"{result}{idx + 1}. {profile['name']}, a character by `{username}` with Level **{rpgtools.xptolevel(profile['xp'])}** (**{profile['xp']}** XP)\n"
         result = discord.Embed(
             title="The Best Players", description=result, colour=0xE7CA01
         )
         await ctx.send(embed=result)
 
-    @commands.command(
-        aliases=["pvp", "battles"],
-        description="Shows the best PvP players. Maximum 10.",
-    )
+    @commands.command(aliases=["pvp", "battles"],)
     async def pvpstats(self, ctx):
+        """Top 10 players by wins in PvP matches."""
         await ctx.trigger_typing()
-        async with self.bot.pool.acquire() as conn:
-            ret = await conn.fetch(
-                'SELECT "user", "name", "pvpwins" from profile ORDER BY "pvpwins" DESC LIMIT 10;'
-            )
-        if ret == []:
-            return await ctx.send(
-                "No character have been created yet. Use `{ctx.prefix}create` to be the first one!"
-            )
+        players = await self.bot.pool.fetch(
+            'SELECT "user", "name", "pvpwins" from profile ORDER BY "pvpwins" DESC LIMIT 10;'
+        )
         result = ""
-        for profile in ret:
-            number = ret.index(profile) + 1
-            charname = await rpgtools.lookup(self.bot, profile[0])
-            pstring = f"{number}. {profile[1]}, a character by `{charname}` with **{profile[2]}** wins\n"
-            result += pstring
+        for idx, profile in enumerate(players):
+            username = await rpgtools.lookup(self.bot, profile["user"])
+            result = f"{result}{idx + 1}. {profile['name']}, a character by `{username}` with **{profile['pvpwins']}** wins\n"
         result = discord.Embed(
             title="The Best PvPers", description=result, colour=0xE7CA01
         )
         await ctx.send(embed=result)
 
-    @commands.command(description="Shows the best lovers.")
+    @commands.command()
     async def lovers(self, ctx):
+        """The top 10 lovers sorted by their spouse's lovescore."""
         await ctx.trigger_typing()
-        async with self.bot.pool.acquire() as conn:
-            ret = await conn.fetch(
-                'SELECT "user", "marriage", "lovescore" FROM profile ORDER BY "lovescore" DESC LIMIT 10;'
-            )
-        if ret == []:
-            return await ctx.send(
-                "No character have been created yet. Use `{ctx.prefix}create` to be the first one!"
-            )
+        players = await self.bot.pool.fetch(
+            'SELECT "user", "marriage", "lovescore" FROM profile ORDER BY "lovescore" DESC LIMIT 10;'
+        )
         result = ""
-        for profile in ret:
-            number = ret.index(profile) + 1
-            user = await rpgtools.lookup(self.bot, profile[0])
-            lover = await rpgtools.lookup(self.bot, profile[1])
-            result += f"**{number}**. **{lover}** gifted their love **{user}** items worth **${profile[2]}**\n"
+        for idx, profile in enumerate(players):
+            lovee = await rpgtools.lookup(self.bot, profile["user"])
+            lover = await rpgtools.lookup(self.bot, profile["marriage"])
+            result = f"{result}**{idx + 1}**. **{lover}** gifted their love **{lovee}** items worth **${profile['lovescore']}**\n"
         result = discord.Embed(
             title="The Best lovers", description=result, colour=0xE7CA01
         )
