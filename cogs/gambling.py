7596cb6f7c34cf71eebdeb83be56b6d4098f2f8f
Jens Reidel <jens@troet.org>
2019-05-04 13:09:46 +0200

    gambling.py refactor needs test.sh

--- cogs/gambling.py
+++ cogs/gambling.py
@@ -5,8 +5,6 @@ Copyright (C) 2018-2019 Diniboy and Gelbpunkt
 This software is dual-licensed under the GNU Affero General Public License for non-commercial and the Travitia License for commercial use.
 For more information, see README.md and LICENSE.md.
 """
-
-
 import asyncio
 import os
 import random
@@ -255,99 +253,80 @@ class BlackJack:
 class Gambling(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.cards = os.listdir("assets/cards")
 
-    @user_cooldown(5)
-    @commands.command(description="Draw a card!", aliases=["card"])
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    @commands.command(aliases=["card"])
     async def draw(self, ctx):
-        await ctx.trigger_typing()
-        files = os.listdir("assets/cards")
-        await ctx.send(file=discord.File(f"assets/cards/{secrets.choice(files)}"))
+        """Draws a random card."""
+        await ctx.send(file=discord.File(f"assets/cards/{secrets.choice(self.cards)}"))
 
     @has_char()
-    @user_cooldown(5)
-    @commands.command(description="Flip a coin to win some money!", aliases=["coin"])
-    async def flip(self, ctx, side: str = "heads", amount: int = 0):
-        side = side.lower()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    @commands.command(aliases=["coin"])
+    async def flip(self, ctx, side: Optional[str.lower] = "heads", *, amount: IntFromTo(0, 100_000) = 0):
+        """Flip a coin and bid on the outcome."""
         if side not in ["heads", "tails"]:
             return await ctx.send(f"Use `heads` or `tails` instead of `{side}`.")
-        if amount < 0:
-            return await ctx.send("Invalid money amount. Must be 0 or higher.")
-        if amount > 100_000:
-            return await ctx.send("You will think of a better way to spend this.")
-        if not await has_money(self.bot, ctx.author.id, amount):
+        if ctx.character_data["money"] < amount:
             return await ctx.send("You are too poor.")
-        result = secrets.choice(["heads", "tails"])
-        if result == "heads":
-            resultemoji = "<:heads:437981551196897281>"
-        else:
-            resultemoji = "<:tails:437981602518138890>"
-        async with self.bot.pool.acquire() as conn:
-            if result == side:
-                await conn.execute(
-                    'UPDATE profile SET money=money+$1 WHERE "user"=$2;',
-                    amount,
-                    ctx.author.id,
-                )
-                await ctx.send(
-                    f"{resultemoji} It's **{result}**! You won **${amount}**!"
-                )
-            else:
-                await conn.execute(
-                    'UPDATE profile SET money=money-$1 WHERE "user"=$2;',
-                    amount,
-                    ctx.author.id,
-                )
-                await ctx.send(
-                    f"{resultemoji} It's **{result}**! You lost **${amount}**!"
-                )
+        result = secrets.choice([("heads", "<:heads:437981551196897281>"), ("tails", "<:tails:437981602518138890>")])
+        if result == side:
+            await self.bot.pool.execute(
+                'UPDATE profile SET money=money+$1 WHERE "user"=$2;',
+                amount,
+                ctx.author.id,
+            )
+            await ctx.send(
+                f"{result[1]} It's **{result[0]}**! You won **${amount}**!"
+            )
+         else:
+            await self.bot.pool.execute(
+                'UPDATE profile SET money=money-$1 WHERE "user"=$2;',
+                amount,
+                ctx.author.id,
+            )
+            await ctx.send(
+                f"{result[1]} It's **{result[0]}**! You lost **${amount}**!"
+            )
 
     @has_char()
-    @user_cooldown(5)
-    @commands.command(description="Roll the dice and win some money!")
-    async def bet(self, ctx, maximum: int = 6, tip: int = 6, money: int = 0):
-        if maximum < 2:
-            return await ctx.send("Invalid Maximum.")
-        if tip > maximum or tip < 1:
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    @commands.command()
+    async def bet(self, ctx, maximum: IntGreaterThan(1) = 6, tip: IntGreaterThan(0) = 6, money: IntFromTo(0, 100_000) = 0):
+        if tip > maximum:
             return await ctx.send(
                 f"Invalid Tip. Must be in the Range of `1` to `{maximum}`."
             )
-        if money < 0:
-            return await ctx.send("Invalid money amount. Must be 0 or higher.")
         if money * (maximum - 1) > 100_000:
             return await ctx.send("Spend it in a better way. C'mon!")
-        if not await has_money(self.bot, ctx.author.id, money):
+        if ctx.character_data["money"] < money:
             return await ctx.send("You're too poor.")
         randomn = secrets.randbelow(maximum + 1)
-        async with self.bot.pool.acquire() as conn:
-            if randomn == tip:
-                await conn.execute(
-                    'UPDATE profile SET money=money+$1 WHERE "user"=$2;',
-                    money * (maximum - 1),
-                    ctx.author.id,
-                )
-                await ctx.send(
-                    f"You won **${money*(maximum-1)}**! The random number was `{randomn}`, you tipped `{tip}`."
-                )
-            else:
-                await conn.execute(
-                    'UPDATE profile SET money=money-$1 WHERE "user"=$2;',
-                    money,
-                    ctx.author.id,
-                )
-                await ctx.send(
-                    f"You lost **${money}**! The random number was `{randomn}`, you tipped `{tip}`."
-                )
-
-    @user_cooldown(15)
-    @commands.command(aliases=["bj"], description="Play some Blackjack")
-    async def blackjack(self, ctx, amount: int):
-        if amount < 0:
-            return await ctx.send("Meh, choose a positive value!")
-        elif amount > 1000:
-            return await ctx.send(
-                "Sorry, due to this being in an experimental status, more than $1000 is not allowed."
+        if randomn == tip:
+            await self.bot.pool.execute(
+                'UPDATE profile SET money=money+$1 WHERE "user"=$2;',
+                money * (maximum - 1),
+                ctx.author.id,
             )
-        if not await has_money(self.bot, ctx.author.id, amount):
+            await ctx.send(
+                f"You won **${money*(maximum-1)}**! The random number was `{randomn}`, you tipped `{tip}`."
+            )
+        else:
+            await self.bot.pool.execute(
+                'UPDATE profile SET money=money-$1 WHERE "user"=$2;',
+                money,
+                ctx.author.id,
+            )
+            await ctx.send(
+                f"You lost **${money}**! The random number was `{randomn}`, you tipped `{tip}`."
+            )
+
+    @commands.cooldown(1, 15, commands.BucketType.user)
+    @commands.command(aliases=["bj"])
+    async def blackjack(self, ctx, amount: IntFromTo(0, 1000)):
+        """[Alpha] Play blackjack against the dealer."""
+        if ctx.character_data["money"] < amount:
             return await ctx.send("You're too poor.")
         bj = BlackJack(ctx, amount)
         await bj.run()
