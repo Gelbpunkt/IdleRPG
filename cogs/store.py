e8ac924b7415cf981a2ff6d1431985fbb7f7a9dd
Jens Reidel <jens@troet.org>
2019-05-16 19:50:41 +0200

    [WIP] booster and adventure remake

--- cogs/store.py
+++ cogs/store.py
@@ -18,8 +18,9 @@ class Store(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(description="The store. Buy boosters here.")
+    @commands.command()
     async def store(self, ctx):
+        """The booster store."""
         shopembed = discord.Embed(
             title="IdleRPG Store",
             description=f"Welcome! Use `{ctx.prefix}purchase storeitemid` to buy something.",
@@ -69,78 +70,49 @@ class Store(commands.Cog):
         )
 
     @has_char()
-    @commands.command(description="View your boosters.")
+    @commands.command()
     async def boosters(self, ctx):
-        async with self.bot.pool.acquire() as conn:
-            boosters = await conn.fetchval(
-                'SELECT (time_booster, luck_booster, money_booster) FROM profile WHERE "user"=$1;',
-                ctx.author.id,
-            )
-            timeboosters = boosters[0]
-            luckboosters = boosters[1]
-            moneyboosters = boosters[2]
-            active = []
-            booster = ["`Time Booster`", "`Luck Booster`", "`Money Booster`"]
-            active = await conn.fetch(
-                'SELECT "type", "end" FROM boosters WHERE "end" > clock_timestamp() AND "user"=$1;',
-                ctx.author.id,
-            )
-        nl = "\n"
+        """View your boosters."""
+        timeboosters = ctx.character_data["time_boosters"]
+        luckboosters = ctx.character_data["luck_boosters"]
+        moneyboosters = ctx.character_data["money_boosters"]
+        time = await self.bot.get_booster(ctx.author, "time")
+        luck = await self.bot.get_booster(ctx.author, "luck")
+        money = await self.bot.get_booster(ctx.author, "money")
+        time = f"Time booster - {str(time).split('.')[0]}" if time else None
+        luck = f"Luck booster - {str(luck).split('.')[0]}" if luck else None
+        money = f"Money booster - {str(money).split('.')[0]}" if money else None
+        actives = "\n".join([b for b in [time, luck, money] if b])
+        if time or luck or money:
+            desc = f"**Currently active**\n{actives}"
+        else:
+            desc = ""
         await ctx.send(
             embed=discord.Embed(
                 title="Your Boosters",
-                description=f"{'**Currently Active**'+nl if active!=[] else ''}{nl.join([booster[row[0]-1]+' active for another '+str(row[1]-datetime.datetime.now(datetime.timezone.utc)).split('.')[0] for row in active])}\n\nTime Boosters: `{timeboosters}`\nLuck Boosters: `{luckboosters}`\nMoney Boosters: `{moneyboosters}`",
+                description=f"{desc}\n\nTime Boosters: `{timeboosters}`\nLuck Boosters: `{luckboosters}`\nMoney Boosters: `{moneyboosters}`",
                 colour=discord.Colour.blurple(),
             ).set_footer(text=f"Use {ctx.prefix}activate to activate one")
         )
 
     @has_char()
-    @commands.command(description="Uses a booster.")
-    async def activate(self, ctx, boostertype: int):
-        if boostertype not in range(1, 4):
+    @commands.command()
+    async def activate(self, ctx, boostertype: str):
+        """Activate a booster."""
+        if boostertype not in ["time", "luck", "money"]:
             return await ctx.send(
                 "That is not a valid booster type. Must be from `1` to `3`."
             )
-        booster = ["time_booster", "luck_booster", "money_booster"][boostertype - 1]
-        async with self.bot.pool.acquire() as conn:
-            res = await conn.fetchval(
-                f'SELECT {booster} FROM profile WHERE "user"=$1;', ctx.author.id
-            )
-            if res == 0:
-                return await ctx.send("You don't have any of these boosters.")
-            check = await conn.fetchrow(
-                'SELECT * FROM boosters WHERE "type"=$1 AND "user"=$2;',
-                boostertype,
-                ctx.author.id,
-            )
-            check2 = await conn.fetchrow(
-                'SELECT * FROM boosters WHERE "type"=$1 AND "user"=$2 AND clock_timestamp() > "end";',
-                boostertype,
-                ctx.author.id,
-            )
-            if check and not check2:
-                return await ctx.send(
-                    f"You already have one of these boosters active! Use `{ctx.prefix}boosters` to see how long it still lasts."
-                )
-            elif check and check2:
-                await conn.execute(
-                    'DELETE FROM boosters WHERE "type"=$1 AND "user"=$2;',
-                    boostertype,
-                    ctx.author.id,
-                )
-            await conn.execute(
-                f'UPDATE profile SET {booster}={booster}-1 WHERE "user"=$1;',
-                ctx.author.id,
-            )
-            end = await conn.fetchval("SELECT clock_timestamp() + interval '1d';")
-            await conn.execute(
-                'INSERT INTO boosters ("user", "type", "end") VALUES ($1, $2, $3);',
-                ctx.author.id,
-                boostertype,
-                end,
-            )
+        boosters = ctx.character_data[f"{boostertype}_booster"]
+        if not boosters:
+            return await ctx.send("You don't have any of these boosters.")
+        check = await self.bot.get_booster(ctx.author, boostertype)
+        if check:
+            return await ctx.send("This booster is already running.")
+        await self.bot.pool.execute(f'UPDATE profile SET "{boostertype}_boosters"="{boostertype}_boosters"-1 WHERE "user"=$1;', ctx.author.id)
+        await self.bot.activate_booster(ctx.author, boostertype)
         await ctx.send(
-            f"Successfully activated a **{booster.replace('_', ' ').capitalize()}** for the next **24 hours**!"
+            f"Successfully activated a **{boostertype.title()} booster** for the next **24 hours**!"
         )
 
 
