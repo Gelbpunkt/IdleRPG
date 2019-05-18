01816c70a0b92fc5155c1210a0c0c6a52e669e23
Jens Reidel <jens@troet.org>
2019-05-18 19:53:59 +0200

    Remove unneeded funcs

--- utils/misc.py
+++ utils/misc.py
@@ -82,26 +82,6 @@ def calcchance(sword, shield, dungeon, level, returnsuccess=False, booster=False
         return randomn <= success
 
 
-def makebg(background, imgtype):
-    with Image.open(background).convert("RGBA").resize(
-        (800, 600), resample=Image.NEAREST
-    ) as bg:
-        if imgtype == 1:
-            with Image.open("assets/profiles/Foreground.png") as fg:
-                bg = Image.alpha_composite(bg, fg)
-                output_buffer = io.BytesIO()
-                bg.save(output_buffer, "png")
-                output_buffer.seek(0)
-                return output_buffer
-        elif imgtype == 2:
-            with Image.open("assets/profiles/Foreground2.png") as fg:
-                bg = Image.alpha_composite(bg, fg)
-                output_buffer = io.BytesIO()
-                bg.save(output_buffer, "png")
-                output_buffer.seek(0)
-                return output_buffer
-
-
 def makeadventures(percentages):
     images = []
 
@@ -130,98 +110,6 @@ def makeadventures(percentages):
             output_buffer.seek(0)
             images.append(output_buffer)
     return images
-
-
-def profile_image(
-    profile,
-    sword,
-    shield,
-    mission,
-    missionend,
-    ranks,
-    color,
-    image,
-    marriage,
-    guild,
-    extras,
-):
-    level = xptolevel(profile[3])
-    try:
-        if not mission[2]:
-            endstr = f"{str(missionend).split('.')[0]} left"
-        else:
-            endstr = "finished"
-        missionnumber = mission[0]
-        missionstring = f"Adventure {missionnumber}, {endstr}"
-    except (KeyError, IndexError, TypeError):
-        missionstring = "You are in no adventure!"
-    guild = guild or "No guild"
-
-    with Image.open(image) as my_image:
-        if color:
-            try:
-                color = hex_to_rgb(color)
-            except ValueError:
-                color = (255, 255, 255)
-        else:
-            color = (255, 255, 255)
-        draw = ImageDraw.Draw(my_image)
-        font = ImageFont.truetype("assets/fonts/CaviarDreams.ttf", 34)
-        font2 = ImageFont.truetype("assets/fonts/CaviarDreams.ttf", 24)
-        font3 = ImageFont.truetype("assets/fonts/CaviarDreams.ttf", 28)
-        draw.text((73, 21), profile[1], color, font=font)
-        draw.text((742, 34), str(level), (0, 0, 0), font=font2)
-        draw.text((98, 315), str(profile[2]), color, font=font)
-        draw.text((98, 386), str(profile[4]), color, font=font)
-        draw.text((81, 138), sword[0], color, font=font2)
-        draw.text((487, 130), str(sword[1]), color, font=font3)
-        draw.text((81, 224), shield[0], color, font=font2)
-        draw.text((487, 221), str(shield[1]), color, font=font3)
-        draw.text((271, 457), str(ranks[0]), color, font=font)
-        draw.text((419, 457), str(ranks[1]), color, font=font)
-        draw.text((264, 570.8), missionstring, color, font=font2)
-        draw.text((589, 351), marriage, color, font=font2)
-        draw.text((589, 444), guild, color, font=font2)
-        draw.text((127, 517), profile[13], color, font=font2)
-        if int(extras[0]) != 0:
-            draw.text((570, 135), f"(+{int(extras[0])})", color, font=font2)
-        if int(extras[1]) != 0:
-            draw.text((570, 223), f"(+{int(extras[1])})", color, font=font2)
-        if profile[13] in [
-            "Mage",
-            "Wizard",
-            "Pyromancer",
-            "Elementalist",
-            "Dark Caster",
-        ]:
-            with Image.open(Path("assets/icons/elementalist.png")).resize(
-                (50, 50), resample=Image.NEAREST
-            ) as overlay:
-                my_image.paste(overlay, (723, 506), overlay)
-        elif profile[13] in ["Thief", "Rogue", "Chunin", "Renegade", "Assassin"]:
-            with Image.open(Path("assets/icons/thief.png")).resize(
-                (50, 50), resample=Image.NEAREST
-            ) as overlay:
-                my_image.paste(overlay, (723, 506), overlay)
-        elif profile[13] in ["Warrior", "Swordsman", "Knight", "Warlord", "Berserker"]:
-            with Image.open(Path("assets/icons/warrior.png")).resize(
-                (50, 50), resample=Image.NEAREST
-            ) as overlay:
-                my_image.paste(overlay, (723, 506), overlay)
-        elif profile[13] in ["Novice", "Proficient", "Artisan", "Master", "Paragon"]:
-            with Image.open(Path("assets/icons/paragon.png")).resize(
-                (50, 50), resample=Image.NEAREST
-            ) as overlay:
-                my_image.paste(overlay, (723, 506), overlay)
-        elif profile[13] in ["Caretaker", "Trainer", "Bowman", "Hunter", "Ranger"]:
-            with Image.open(Path("assets/icons/ranger.png")).resize(
-                (50, 50), resample=Image.NEAREST
-            ) as overlay:
-                my_image.paste(overlay, (723, 506), overlay)
-        output_buffer = io.BytesIO()
-        my_image.save(output_buffer, "png")
-        output_buffer.seek(0)
-        return output_buffer
 
 
 async def lookup(bot, userid):
