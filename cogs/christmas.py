bd5907c3a2f90c60f7b35b519493a84729d560d1
Jens Reidel <jens@troet.org>
2019-05-24 19:04:50 +0200

    time for a james bond

--- cogs/christmas.py
+++ cogs/christmas.py
@@ -5,8 +5,6 @@ Copyright (C) 2018-2019 Diniboy and Gelbpunkt
 This software is dual-licensed under the GNU Affero General Public License for non-commercial and the Travitia License for commercial use.
 For more information, see README.md and LICENSE.md.
 """
-
-
 import asyncio
 import datetime
 import random
@@ -56,38 +54,38 @@ class Christmas(commands.Cog):
 
     @commands.group(invoke_without_command=True)
     async def calendar(self, ctx):
-        """Look at your Winter Calendar"""
+        _("""Look at your Winter Calendar""")
         today = datetime.datetime.now().day
         if today > 25 or today < 1:
-            return await ctx.send("No calendar to show!")
-        await ctx.send(file=discord.File(f"calendar/Day {today - 1}.png"))
+            return await ctx.send(_("No calendar to show!"))
+        await ctx.send(file=discord.File(f"assets/calendar/Day {today - 1}.png"))
 
     @has_char()
     @user_cooldown(86401)  # truly make sure they use it once a day
     @calendar.command(name="open")
     async def _open(self, ctx):
-        """Open the Winter Calendar once every day."""
+        _("""Open the Winter Calendar once every day.""")
         today = datetime.datetime.now().date()
         christmas_too_late = datetime.date(2018, 12, 25)
         first_dec = datetime.date(2018, 12, 1)
         if today >= christmas_too_late or today < first_dec:
-            return await ctx.send("It's not calendar time yet...")
+            return await ctx.send(_("It's not calendar time yet..."))
         reward = rewards[today.day]
-        reward_text = f"**You opened day {today.day}!**"
+        reward_text = _("**You opened day {today}!**").format(today=today.day)
         async with self.bot.pool.acquire() as conn:
             if reward["puzzle"]:
                 await conn.execute(
                     'UPDATE profile SET puzzles=puzzles+1 WHERE "user"=$1;',
                     ctx.author.id,
                 )
-                reward_text = f"{reward_text}\n- A mysterious puzzle piece"
+                reward_text = f"{reward_text}\n- {_('A mysterious puzzle piece')}"
             if reward["crates"]:
                 await conn.execute(
                     'UPDATE profile SET crates=crates+$1 WHERE "user"=$2;',
                     reward["crates"],
                     ctx.author.id,
                 )
-                reward_text = f"{reward_text}\n- {reward['crates']} crates"
+                reward_text = f"{reward_text}\n- {reward['crates']} {_('crates')}"
             if reward["money"]:
                 await conn.execute(
                     'UPDATE profile SET money=money+$1 WHERE "user"=$2;',
@@ -101,24 +99,22 @@ class Christmas(commands.Cog):
                     "https://i.imgur.com/HuF0VbN.png",
                     ctx.author.id,
                 )
-                reward_text = f"{reward_text}\n- A special surprise - check out `{ctx.prefix}eventbackground` for a new Wintersday background!"
+                reward_text = f"{reward_text}\n- {_('A special surprise - check out `{prefix}eventbackground` for a new Wintersday background!').format(prefix=ctx.prefix)}"
         await ctx.send(reward_text)
 
     @has_char()
     @commands.command()
     async def combine(self, ctx):
-        """Combine the mysterious puzzle pieces."""
+        _("""Combine the mysterious puzzle pieces.""")
         async with self.bot.pool.acquire() as conn:
             if (
-                not (
-                    await conn.fetchval(
-                        'SELECT puzzles FROM profile WHERE "user"=$1;', ctx.author.id
-                    )
+                await conn.fetchval(
+                    'SELECT puzzles FROM profile WHERE "user"=$1;', ctx.author.id
                 )
-                == 6
+                != 6
             ):
                 return await ctx.send(
-                    "The mysterious puzzles don't fit together... Maybe some are missing?"
+                    _("The mysterious puzzles don't fit together... Maybe some are missing?")
                 )
             bg = random.choice(
                 [
@@ -139,15 +135,15 @@ class Christmas(commands.Cog):
                 'UPDATE profile SET puzzles=0 WHERE "user"=$1;', ctx.author.id
             )
         await ctx.send(
-            f"You combined the puzzles! In your head a voice whispers: *Well done. Now use `{ctx.prefix}eventbackground 1` to set your new background that you just acquired...*"
+            _("You combined the puzzles! In your head a voice whispers: *Well done. Now use `{prefix}eventbackground 1` to set your new background that you just acquired...*").format(prefix=ctx.prefix)
         )
 
     @is_guild_officer()
     @commands.command()
     async def snowballfight(self, ctx, enemy: discord.Member):
-        """Make a snowball fights against another guild."""
+        _("""Make a snowball fights against another guild.""")
         if enemy is ctx.author:
-            return await ctx.send("C'mon!")
+            return await ctx.send(_("You may not fight yourself."))
         async with self.bot.pool.acquire() as conn:
             guild1, rank1 = await conn.fetchval(
                 'SELECT (guild, guildrank) FROM profile WHERE "user"=$1;', ctx.author.id
@@ -156,7 +152,7 @@ class Christmas(commands.Cog):
                 'SELECT (guild, guildrank) FROM profile WHERE "user"=$1;', enemy.id
             )
             if rank2 == "Member":
-                return await ctx.send("The enemy must be an officer or higher.")
+                return await ctx.send(_("The enemy must be an officer or higher."))
             guild1 = await conn.fetchrow('SELECT * FROM guild WHERE "id"=$1;', guild1)
             guild2 = await conn.fetchrow('SELECT * FROM guild WHERE "id"=$1;', guild2)
             guild1_members = [
@@ -172,19 +168,19 @@ class Christmas(commands.Cog):
                 )
             ]
 
-        def check(msg):
-            return (
-                msg.author.id == enemy.id
-                and msg.content.lower() == "snowballfight accept"
-            )
-
-        await ctx.send(
-            f"{enemy.mention}, {ctx.author.mention} has challenged you for an epic snowball fight! If you want to accept, type `snowballfight accept`\n**IMPORTANT: This is very spammy, make sure you are using a dedicated channel!**"
+        msg = await ctx.send(
+            _("{enemy}, {author} has challenged you for an epic snowball fight! If you want to accept, react \U00002744\n**IMPORTANT: This is very spammy, make sure you are using a dedicated channel!**").format(enemy=enemy.mention, author=ctx.author.mention)
         )
+
+        def check(r, u):
+            return r.message.id == msg.id and u == enemy and str(r.emoji) == "\U00002744"
+
+        await msg.add_reaction("\U00002744")
+
         try:
-            await self.bot.wait_for("message", check=check, timeout=30)
+            await self.bot.wait_for("reaction_add", check=check, timeout=30)
         except asyncio.TimeoutError:
-            return await ctx.send("Timed out...")
+            return await ctx.send(_("Timed out..."))
         conv = commands.UserConverter()
         team1 = [ctx.author]
         team2 = [enemy]
@@ -201,39 +197,39 @@ class Christmas(commands.Cog):
             )
 
         await ctx.send(
-            f"{ctx.author.mention}, use `snowballfight nominate @user` to add one of your guild members to the fight!"
+            _("{author}, type `snowballfight nominate @user` to add one of your guild members to the fight!").format(author=ctx.author.mention)
         )
         while len(team1) == 1:
             try:
                 msg = await self.bot.wait_for("message", check=check1, timeout=30)
             except asyncio.TimeoutError:
-                return await ctx.send("Timed out...")
+                return await ctx.send(_("Timed out..."))
             try:
                 u = await conv.convert(ctx, msg.content.split()[-1])
             except commands.BadArgument:
                 continue
             if u.id not in guild1_members:
-                await ctx.send("Not one of your guild members...")
+                await ctx.send(_("Not one of your guild members..."))
             elif u in team1:
-                await ctx.send("That's you!")
+                await ctx.send(_("That's you!"))
             else:
                 team1.append(u)
         await ctx.send(
-            f"{enemy.mention}, use `snowballfight nominate @user` to add one of your guild members to the fight!"
+            _("{enemy}, use `snowballfight nominate @user` to add one of your guild members to the fight!").format(enemy=enemy.mention)
         )
         while len(team2) == 1:
             try:
                 msg = await self.bot.wait_for("message", check=check2, timeout=30)
             except asyncio.TimeoutError:
-                return await ctx.send("Timed out...")
+                return await ctx.send(_("Timed out..."))
             try:
                 u = await conv.convert(ctx, msg.content.split()[-1])
             except commands.BadArgument:
                 continue
             if u.id not in guild2_members:
-                await ctx.send("Not one of your guild members...")
+                await ctx.send(_("Not one of your guild members..."))
             elif u in team2:
-                await ctx.send("That's you!")
+                await ctx.send(_("That's you!"))
             else:
                 team2.append(u)
         points1 = 1
@@ -250,12 +246,12 @@ class Christmas(commands.Cog):
                 + ":black_large_square:" * (10 - points2)
             )
             await ctx.send(
-                f"""
-{ctx.author.mention}'s team vs {enemy.mention}'s team
-{t1} - {ctx.author}
+                _("""
+{author.mention}'s team vs {enemy.mention}'s team
+{t1} - {author}
 {t2} - {enemy}
 Next round starts in 5 seconds!
-"""
+""").format(author=ctx.author, enemy=enemy, t1=t1, t2=t2)
             )
             await asyncio.sleep(5)
             game_mode = random.choice(["typeit", "maths", "hangman"])
@@ -271,7 +267,7 @@ Next round starts in 5 seconds!
                     ) and msg.content == real
 
                 await ctx.send(
-                    "It's word typing time! In 3 seconds, I will send a word. Whoever types it fastest gets one point!"
+                    _("It's word typing time! In 3 seconds, I will send a word. Whoever types it fastest gets one point!")
                 )
                 await asyncio.sleep(3)
                 await ctx.send(f"`{word}`")
@@ -279,13 +275,13 @@ Next round starts in 5 seconds!
                     msg = await self.bot.wait_for("message", check=corr, timeout=45)
                 except asyncio.TimeoutError:
                     return await ctx.send(
-                        "Noone managed to get it right, I'll cancel the fight!"
+                        _("Noone managed to get it right, I'll cancel the fight!")
                     )
                 if msg.author in team1:
                     points1 += 1
                 else:
                     points2 += 1
-                await ctx.send(f"{msg.author} got it right!")
+                await ctx.send(_("{author} got it right!").format(author=ctx.author.mention))
             elif game_mode == "maths":
                 m = random.randint(1, 20)
                 x = random.randint(1, 30)
@@ -303,7 +299,7 @@ Next round starts in 5 seconds!
                     ) and msg.content == res
 
                 await ctx.send(
-                    "It's maths time! In 3 seconds, I'll send a simple maths task to solve! Type the answer to get a point!"
+                    _("It's maths time! In 3 seconds, I'll send a simple maths task to solve! Type the answer to get a point!")
                 )
                 await asyncio.sleep(3)
                 await ctx.send(f"`({m} * {x}) / {c} + {d}`")
@@ -311,13 +307,13 @@ Next round starts in 5 seconds!
                     msg = await self.bot.wait_for("message", check=corr, timeout=45)
                 except asyncio.TimeoutError:
                     return await ctx.send(
-                        "Noone managed to get it right, I'll cancel the fight!"
+                        _("Noone managed to get it right, I'll cancel the fight!")
                     )
                 if msg.author in team1:
                     points1 += 1
                 else:
                     points2 += 1
-                await ctx.send(f"{msg.author} got it right!")
+                await ctx.send(_("{user} got it right!").format(user=msg.author))
             elif game_mode == "hangman":
                 word = random.choice(self.words).strip()
 
@@ -329,7 +325,7 @@ Next round starts in 5 seconds!
                 disp = "_ " * len(word)
                 guessed = []
                 await ctx.send(
-                    "It's hangman time! In 3 seconds, I'll send a hangman-style word and you will have to either send your full word as the guess or a letter to check for!"
+                    _("It's hangman time! In 3 seconds, I'll send a hangman-style word and you will have to either send your full word as the guess or a letter to check for!")
                 )
                 await asyncio.sleep(3)
                 q = await ctx.send(f"`{disp}`")
@@ -338,14 +334,14 @@ Next round starts in 5 seconds!
                         msg = await self.bot.wait_for("message", check=corr, timeout=20)
                     except asyncio.TimeoutError:
                         return await ctx.send(
-                            "Noone participated, I'll cancel the fight!"
+                            _("Noone participated, I'll cancel the fight!")
                         )
                     if msg.content == word:
                         if msg.author in team1:
                             points1 += 1
                         else:
                             points2 += 1
-                        await ctx.send(f"{msg.author} got it right!")
+                        await ctx.send(_("{user} got it right!").format(user=msg.author))
                         break
                     else:
                         try:
@@ -361,16 +357,16 @@ Next round starts in 5 seconds!
                         disp = " ".join([i if (i in guessed) else "_" for i in word])
                         await q.edit(content=f"`{disp}`")
         if points1 > points2:
-            await ctx.send(f"Team 1 ({ctx.author.mention}) won!")
+            await ctx.send(_("Team 1 ({user}) won!").format(user=ctx.author.mention))
         else:
-            await ctx.send(f"Team 2 ({enemy.mention}) won!")
+            await ctx.send(_("Team 2 ({user}) won!").format(user=enemy.mention))
 
     @is_guild_leader()
     @commands.command()
     async def signup(self, ctx):
-        """Sign up for the snowball tournament."""
+        _("""Sign up for the snowball tournament.""")
         if datetime.datetime.now().day >= 6:
-            return await ctx.send("Too late, my friend.")
+            return await ctx.send(_("Too late, my friend."))
         g = await self.bot.pool.fetchrow(
             'SELECT * FROM guild WHERE "leader"=$1;', ctx.author.id
         )
