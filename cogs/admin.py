a93b8b407ca5450be5c2613493709c1ab9e4fa07
Jens Reidel <jens@troet.org>
2019-05-24 18:12:20 +0200

    underscore'd admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -20,12 +20,14 @@ class Admin(commands.Cog):
     @is_admin()
     @commands.command(aliases=["agive"], hidden=True)
     async def admingive(self, ctx, money: int, other: UserWithCharacter):
-        """[Bot Admin only] Gives money to a user without loss."""
+        _("""[Bot Admin only] Gives money to a user without loss.""")
         await self.bot.pool.execute(
             'UPDATE profile SET money=money+$1 WHERE "user"=$2;', money, other.id
         )
         await ctx.send(
-            f"Successfully gave **${money}** without a loss for you to **{other}**."
+            _(
+                "Successfully gave **${money}** without a loss for you to **{other}**."
+            ).format(money=money, other=other)
         )
         await self.bot.http.send_message(
             self.bot.config.admin_log_channel,
@@ -35,11 +37,15 @@ class Admin(commands.Cog):
     @is_admin()
     @commands.command(aliases=["aremove"], hidden=True)
     async def adminremove(self, ctx, money: int, other: UserWithCharacter):
-        """[Bot Admin only] Removes money from a user without gain."""
+        _("""[Bot Admin only] Removes money from a user without gain.""")
         await self.bot.pool.execute(
             'UPDATE profile SET money=money-$1 WHERE "user"=$2;', money, other.id
         )
-        await ctx.send(f"Successfully removed **${money}** from **{other}**.")
+        await ctx.send(
+            _("Successfully removed **${money}** from **{other}**.").format(
+                money=money, other=other
+            )
+        )
         await self.bot.http.send_message(
             self.bot.config.admin_log_channel,
             f"**{ctx.author}** removed **${money}** from **{other}**.",
@@ -48,11 +54,11 @@ class Admin(commands.Cog):
     @is_admin()
     @commands.command(aliases=["adelete"], hidden=True)
     async def admindelete(self, ctx, other: UserWithCharacter):
-        """[Bot Admin only] Deletes any user's account."""
+        _("""[Bot Admin only] Deletes any user's account.""")
         if other.id in ctx.bot.config.admins:  # preserve deletion of admins
-            return await ctx.send("Very funny...")
+            return await ctx.send(_("Very funny..."))
         await self.bot.pool.execute('DELETE FROM profile WHERE "user"=$1;', other.id)
-        await ctx.send("Successfully deleted the character.")
+        await ctx.send(_("Successfully deleted the character."))
         await self.bot.http.send_message(
             self.bot.config.admin_log_channel, f"**{ctx.author}** deleted **{other}**."
         )
@@ -60,11 +66,13 @@ class Admin(commands.Cog):
     @is_admin()
     @commands.command(aliases=["arename"], hidden=True)
     async def adminrename(self, ctx, target: UserWithCharacter):
-        """[Bot Admin only] Renames a character."""
+        _("""[Bot Admin only] Renames a character.""")
         if target.id in ctx.bot.config.admins:  # preserve renaming of admins
-            return await ctx.send("Very funny...")
+            return await ctx.send(_("Very funny..."))
 
-        await ctx.send("What shall the character's name be? (min. 3 letters, max. 20)")
+        await ctx.send(
+            _("What shall the character's name be? (min. 3 letters, max. 20)")
+        )
 
         def mycheck(amsg):
             return (
@@ -77,12 +85,12 @@ class Admin(commands.Cog):
         try:
             name = await self.bot.wait_for("message", timeout=60, check=mycheck)
         except asyncio.TimeoutError:
-            return await ctx.send("Timeout expired.")
+            return await ctx.send(_("Timeout expired."))
 
         await self.bot.pool.execute(
             'UPDATE profile SET "name"=$1 WHERE "user"=$2;', name.content, target.id
         )
-        await ctx.send("Renamed.")
+        await ctx.send(_("Renamed."))
         await self.bot.http.send_message(
             self.bot.config.admin_log_channel,
             f"**{ctx.author}** renamed **{target}** to **{name}**.",
@@ -91,13 +99,17 @@ class Admin(commands.Cog):
     @is_admin()
     @commands.command(aliases=["acrate"], hidden=True)
     async def admincrate(self, ctx, target: UserWithCharacter, amount: int = 1):
-        """[Bot Admin only] Gives/removes crates to a user without loss."""
+        _("""[Bot Admin only] Gives/removes crates to a user without loss.""")
         await self.bot.pool.execute(
             'UPDATE profile SET "crates"="crates"+$1 WHERE "user"=$2;',
             amount,
             target.id,
         )
-        await ctx.send(f"Successfully gave **{amount}** crates to **{target}**.")
+        await ctx.send(
+            _("Successfully gave **{amount}** crates to **{target}**.").format(
+                amount=amount, target=target
+            )
+        )
         await self.bot.http.send_message(
             self.bot.config.admin_log_channel,
             f"**{ctx.author}** gave **{amount}** crates to **{target}**.",
@@ -106,11 +118,15 @@ class Admin(commands.Cog):
     @is_admin()
     @commands.command(aliases=["axp"], hidden=True)
     async def adminxp(self, ctx, target: UserWithCharacter, amount: int):
-        """[Bot Admin only] Gives xp to a user."""
+        _("""[Bot Admin only] Gives xp to a user.""")
         await self.bot.pool.execute(
             'UPDATE profile SET "xp"="xp"+$1 WHERE "user"=$2;', amount, target.id
         )
-        await ctx.send(f"Successfully gave **{amount}** XP to **{target}**.")
+        await ctx.send(
+            _("Successfully gave **{amount}** XP to **{target}**.").format(
+                amount=amount, target=target
+            )
+        )
         await self.bot.http.send_message(
             self.bot.config.admin_log_channel,
             f"**{ctx.author}** gave **{amount}** XP to **{target}**.",
@@ -119,7 +135,7 @@ class Admin(commands.Cog):
     @is_admin()
     @commands.command(aliases=["awipeperks"], hidden=True)
     async def adminwipeperks(self, ctx, target: UserWithCharacter):
-        """[Bot Admin only] Wipes someone's donator perks."""
+        _("""[Bot Admin only] Wipes someone's donator perks.""")
         async with self.bot.pool.acquire() as conn:
             await conn.execute(
                 'UPDATE profile SET "background"=$1, "class"=$2 WHERE "user"=$3;',
@@ -144,7 +160,9 @@ class Admin(commands.Cog):
             )
 
         await ctx.send(
-            f"Successfully reset {target}'s background, class, item names and guild member limit."
+            _(
+                "Successfully reset {target}'s background, class, item names and guild member limit."
+            ).format(target=target)
         )
         await self.bot.http.send_message(
             self.bot.config.admin_log_channel,
