ab948b62ec0a4673445d2b422692b534c09e1e8c
Jens <jens@troet.org>
2019-05-30 09:49:11 +0000

    fix

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -135,7 +135,7 @@ class Errorhandler(commands.Cog):
                         "An argument or value you entered was far too high for me to handle properly!"
                     )
                 )
-            elif isinstance(error.original, LookupError);
+            elif isinstance(error.original, LookupError):
                 return await ctx.send)
                     _(
                         "The languages have been reloaded while you were using a command. The execution therefore had to be stopped. Please try again."
