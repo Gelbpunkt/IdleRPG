7e6e6ea5a4d49d461bde72ca834a3c6a08c4c996
Jens <jens@troet.org>
2019-07-11 07:04:28 +0000

    Add the crate opening logic

--- cogs/crates.py
+++ cogs/crates.py
@@ -16,6 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import random
+from collections import namedtuple
 
 import discord
 from discord.ext import commands
@@ -27,6 +28,13 @@ from utils.checks import has_char
 class Crates(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.emotes = namedtuple("CrateEmotes", "common uncommon rare magic legendary")(
+            common="<:CrateCommon:598094865666015232>",
+            uncommon="<:CrateUncommon:598094865397579797>",
+            rare="<:CrateRare:598094865485791233>",
+            magic="<:CrateMagic:598094865611358209>",
+            legendary="<:CrateLegendary:598094865678598144>",
+        )
 
     @has_char()
     @commands.command(aliases=["boxes"])
@@ -35,9 +43,23 @@ class Crates(commands.Cog):
         _("""Shows your crates.""")
         await ctx.send(
             _(
-                "You currently have **{crates}** crates, {author}! Use `{prefix}open` to open one!"
+                """\
+**{author}'s crates**
+
+{emotes.common} {common}
+{emotes.uncommon} {uncommon}
+{emotes.rare} {rare}
+{emotes.magic} {magic}
+{emotes.legendary} {legendary}
+
+ Use `{prefix}open [rarity]` to open one!"""
             ).format(
-                crates=ctx.character_data["crates"],
+                emotes=self.emotes,
+                common=ctx.character_data["crates_common"],
+                uncommon=ctx.character_data["crates_uncommon"],
+                rare=ctx.character_data["crates_rare"],
+                magic=ctx.character_data["crates_magic"],
+                legendary=ctx.character_data["legendary"],
                 author=ctx.author.mention,
                 prefix=ctx.prefix,
             )
@@ -46,37 +68,62 @@ class Crates(commands.Cog):
     @has_char()
     @commands.command(name="open")
     @locale_doc
-    async def _open(self, ctx):
+    async def _open(self, ctx, rarity: str.lower = "common"):
         _("""Opens a crate.""")
-        if ctx.character_data["crates"] < 1:
+        if rarity not in ["common", "uncommon", "rare", "magic", "legendary"]:
+            return await ctx.send(
+                _("{rarity} is not a valid rarity.").format(rarity=rarity)
+            )
+        if ctx.character_data[f"crates_{rarity}"] < 1:
             return await ctx.send(
                 _(
-                    "Seems like you don't have a crate yet. Vote me up to get some or earn them!"
+                    "Seems like you don't have any crate of this rarity yet. Vote me up to get a random one or find them!"
                 )
             )
-        rand = random.randint(1, 6)
-        if rand == 1:
-            minstat, maxstat = (20, 30)
-        elif rand == 2 or rand == 3:
-            minstat, maxstat = (10, 19)
-        else:
-            minstat, maxstat = (1, 9)
-
-        luck_multiply = ctx.character_data["luck"]
-        minstat, maxstat = (
-            round(minstat * luck_multiply),
-            round(maxstat * luck_multiply),
-        )
+        # A number to detemine the crate item range
+        rand = random.randint(1, 10)
+        if rarity == "common":
+            if rand < 2:  # 20% 20-30
+                minstat, maxstat = (20, 30)
+            elif rand < 5:  # 30% 10-19
+                minstat, maxstat = (10, 19)
+            else:  # 50% 1-9
+                minstat, maxstat = (1, 9)
+        elif rarity == "uncommon":
+            if rand < 2:  # 20% 30-35
+                minstat, maxstat = (30, 35)
+            elif rand < 5:  # 30% 20-29
+                minstat, maxstat = (20, 29)
+            else:  # 50% 10-19
+                minstat, maxstat = (10, 19)
+        elif rarity == "rare":
+            if rand < 2:  # 20% 35-40
+                minstat, maxstat = (35, 40)
+            elif rand < 5:  # 30% 30-34
+                minstat, maxstat = (30, 34)
+            else:  # 50% 20-29
+                minstat, maxstat = (20, 29)
+        elif rarity == "magic":
+            if rand < 2:  # 20% 41-45
+                minstat, maxstat = (41, 45)
+            elif rand < 5:  # 30% 35-40
+                minstat, maxstat = (35, 40)
+            else:
+                minstat, maxstat = (30, 34)
+        elif rarity == "legendary":  # no else because why
+            if rand < 2:  # 20% 48-50
+                minstat, maxstat = (48, 50)
+            elif rand < 5:  # 30% 45-48
+                minstat, maxstat = (45, 48)
+            else:  # 50% 41-45
+                minstat, maxstat = (41, 45)
 
         item = await self.bot.create_random_item(
-            minstat=minstat if minstat < 30 else 30,
-            maxstat=maxstat if maxstat < 30 else maxstat,
-            minvalue=1,
-            maxvalue=250,
-            owner=ctx.author,
+            minstat=minstat, maxstat=maxstat, minvalue=1, maxvalue=250, owner=ctx.author
         )
         await self.bot.pool.execute(
-            'UPDATE profile SET "crates"="crates"-1 WHERE "user"=$1;', ctx.author.id
+            f'UPDATE profile SET "crates_{rarity}"="crates_{rarity}"-1 WHERE "user"=$1;',
+            ctx.author.id,
         )
         embed = discord.Embed(
             title=_("You gained an item!"),
@@ -91,8 +138,8 @@ class Crates(commands.Cog):
         embed.add_field(name=_("Armor"), value=item["armor"], inline=True)
         embed.add_field(name=_("Value"), value=f"${item['value']}", inline=False)
         embed.set_footer(
-            text=_("Remaining crates: {crates}").format(
-                crates=ctx.character_data["crates"] - 1
+            text=_("Remaining {rarity} crates: {crates}").format(
+                crates=ctx.character_data[f"crates_{rarity}"] - 1, rarity=rarity
             )
         )
         await ctx.send(embed=embed)
