43f1b0800976aa8d99c5036b628b7791c53cbb7c
Jens <jens@troet.org>
2019-08-26 18:21:48 +0000

    Fix Ananke

--- cogs/raid.py
+++ cogs/raid.py
@@ -1699,7 +1699,7 @@ Quick and ugly: <https://discordapp.com/oauth2/authorize?client_id=4539639655219
             await asyncio.sleep(4)
 
         if boss["hp"] < 1:
-            winner = random.choice(raid)
+            winner = random.choice(list(raid.keys()))
             await self.bot.pool.execute(
                 'UPDATE profile SET "crates_legendary"="crates_legendary"+1 WHERE "user"=$1;',
                 winner.id,
