cd150b0bdc80a6c07c21eb3ae4ea02e572249c74
CHamburr <cenhan@outlook.com>
2019-09-03 18:07:05 +0800

    Fix exploit of bot auto confirming
    The reaction that the bot adds to the message triggers the listener and the bot thinks that it confirmed.

--- classes/context.py
+++ classes/context.py
@@ -48,6 +48,9 @@ class Context(commands.Context):
     ):
         user = user or self.author
         emojis = (emoji_no, emoji_yes)
+        
+        if user.id == self.bot.user.id:
+            return false
 
         msg = await self.send(
             embed=discord.Embed(
