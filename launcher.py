6b750213913bf76ac4cf560f4c66a1cbf4451950
Diniboy1123 <Diniboy1123@users.noreply.github.com>
2019-09-15 19:14:38 +0200

    Hotfix

--- launcher.py
+++ launcher.py
@@ -167,7 +167,7 @@ class Main:
             print_exc()
             exit("[ERROR] Redis must be installed properly")
 
-        await self.bot.redis.execute_pubsub("SUBSCRIBE", self.communication_channel)
+        await self.bot.redis.execute_pubsub("SUBSCRIBE", shard_announce_channel)
         channel = self.redis.pubsub_channels[bytes(shard_announce_channel, "utf-8")]
         while await channel.wait_message():
             try:
