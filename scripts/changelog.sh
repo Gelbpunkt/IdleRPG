00f1d210324b20f1f5e18c0fb30c8200dc1da4d0
Jens Reidel <jens@troet.org>
2019-10-31 11:46:34 -0700

    Tweak the changelog script

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 # add git-chglog to path before
-git-chglog -o CHANGELOG.md git-chglog --next-tag $1
+git-chglog --next-tag $1 -o NEXT_CHANGELOG.md $1
+read -p "Done, please now hit enter, modify it and then I will add it to the changelog file"
+vim NEXT_CHANGELOG.md
+cat <(cat NEXT_CHANGELOG.md) CHANGELOG.md > CHANGELOG.tmp
+rm CHANGELOG.md
+mv CHANGELOG.tmp CHANGELOG.md
