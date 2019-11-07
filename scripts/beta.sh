b8edfab2a2174300e79136f8cefdf8ce080228b4
Jens Reidel <jens@troet.org>
2019-11-07 19:17:04 +0100

    Fix beta script

--- scripts/beta.sh
+++ scripts/beta.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 # Adrian's script for setting up a quick test deployment
-podman pod create idlerpg
-podman run --rm --pod idlerpg --name redis redis:5-alpine
+podman pod create --name idlerpg
+podman run --rm -d --pod idlerpg --name redis redis:5-alpine
 cat <<EOF > start.sh
-wget https://git.travitia.xyz/Adrian/IdleRPG/raw/v4/schema.sql
-chown postgres schema.sql
-su postgres
 createdb idlerpg
 psql idlerpg -c "CREATE ROLE jens WITH PASSWORD 'owo';"
 psql idlerpg -c "ALTER ROLE jens WITH LOGIN;"
-psql idlerpg -c "GRANT ALL ON SCHEMA public TO jens;"
-psql idlerpg < schema.sql
+psql idlerpg -c "CREATE ROLE prest WITH PASSWORD 'placeholder';"
+psql idlerpg -c "CREATE ROLE votehandler WITH PASSWORD 'placeholder';"
 EOF
-podman run --rm --pod idlerpg --name postgres -v start.sh:/docker-entrypoint-initdb.d/init.sh:z postgres:12-alpine
+podman run --rm -d --pod idlerpg --name postgres -v $(pwd)/start.sh:/docker-entrypoint-initdb.d/init.sh:z -v $(pwd)/schema.sql:/docker-entrypoint-initdb.d/init.sql:z postgres:12-alpine
