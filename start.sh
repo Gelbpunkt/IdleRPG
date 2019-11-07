a130cef30038e0ee66b2fadd699043421ff52d6f
Jens Reidel <jens@troet.org>
2019-11-07 13:37:48 -0800

    Remove unneeded file

--- start.sh
+++
@@ -1,5 +0,0 @@
-createdb idlerpg
-psql idlerpg -c "CREATE ROLE jens WITH PASSWORD 'owo';"
-psql idlerpg -c "ALTER ROLE jens WITH LOGIN;"
-psql idlerpg -c "CREATE ROLE prest WITH PASSWORD 'placeholder';"
-psql idlerpg -c "CREATE ROLE votehandler WITH PASSWORD 'placeholder';"
