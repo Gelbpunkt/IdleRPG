a24a15442727189b82c0239029e3b425c0c38e2d
Jens Reidel <jens@troet.org>
2019-11-07 21:32:20 +0100

    New script to show columns in schema

---
+++ scripts/list_fields.py
@@ -0,0 +1,19 @@
+with open("schema.sql", "r") as f:
+	stuff = f.read().splitlines()
+
+t = ""
+c = []
+in_t = False
+for l in stuff:
+	if l.startswith("CREATE TABLE"):
+		t = l.split()[2].split(".")[1]
+		in_t = True
+		print(t)
+	elif in_t:
+		if l == ");":
+			print(c)
+			t = ""
+			in_t = False
+			c = []
+		else:
+			c.append(l.split()[0].strip('"'))
