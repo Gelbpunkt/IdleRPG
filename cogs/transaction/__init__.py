6c22ca0f6f2a4bf901c95216f9505f0118dcd486
Max <maxhey4@gmail.com>
2019-11-22 06:48:16 +0000

    fix a bug that allowed to add multiple of the same item

--- cogs/transaction/__init__.py
+++ cogs/transaction/__init__.py
@@ -268,6 +268,8 @@ class Transaction(commands.Cog):
     @locale_doc
     async def add_item(self, ctx, itemid: int):
         _("""Adds items to a trade.""")
+        if itemid in [x['id'] for x in ctx.transaction['items']]:
+            return await ctx.send(_("You already added this item!"))
         if (item := await self.bot.has_item(ctx.author.id, itemid)) :
             if item["original_name"] or item["original_type"]:
                 return await ctx.send(_("You may not sell modified items."))
