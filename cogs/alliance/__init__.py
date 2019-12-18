1a94762b7ac426310c3da8a609676a3f364bb3b7
Max <maxhey4@gmail.com>
2019-12-18 06:20:04 +0000

    prevent bots from joining alliance attacks

--- cogs/alliance/__init__.py
+++ cogs/alliance/__init__.py
@@ -524,7 +524,8 @@ class Alliance(commands.Cog):
             try:
                 r, u = await self.bot.wait_for(
                     "reaction_add",
-                    check=lambda r, u: u not in attacking_users
+                    check=lambda r, u: u not in attacking_user
+                    and not u.bot
                     and str(r.emoji) == "\U00002694"
                     and r.message.id == msg.id,
                     timeout=300,
