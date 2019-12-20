5428687b929b4c526548651a0f7a582524323f63
Max <maxhey4@gmail.com>
2019-12-20 20:55:56 +0000

    fix child rename bug

--- cogs/marriage/__init__.py
+++ cogs/marriage/__init__.py
@@ -516,6 +516,7 @@ class Marriage(commands.Cog):
                     msg.author.id in [ctx.author.id, ctx.character_data["marriage"]]
                     and msg.channel.id == ctx.channel.id
                     and len(msg.content) <= 20
+                    and len(msg.content) > 0
                     and msg.content not in names
                 )
 
