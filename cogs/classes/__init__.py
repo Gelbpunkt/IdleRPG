745a6e352ef00b1e3638777b0bf245f87822f418
Jens Reidel <jens@troet.org>
2020-01-15 14:32:22 -0800

    Class name idea

--- cogs/classes/__init__.py
+++ cogs/classes/__init__.py
@@ -241,15 +241,15 @@ class Classes(commands.Cog):
         _("""Evolve tree.""")
         await ctx.send(
             """```
-Level 0   |  Level 5    |  Level 10     | Level 15        |  Level 20
-----------------------------------------------------------------------
-Warriors ->  Swordsmen ->  Knights     -> Warlords       ->  Berserker
-Thieves  ->  Rogues    ->  Chunin      -> Renegades      ->  Assassins
-Mage     ->  Wizards   ->  Pyromancers -> Elementalists  ->  Dark Caster
-Novice   ->  Proficient->  Artisan     -> Master         ->  Paragon
-Caretaker->  Trainer   ->  Bowman      -> Hunter         ->  Ranger
-Stabber  ->  Fighter   ->  Hero        -> Dragonslayer   ->  Raider
-Priest   ->  Mysticist ->  Summoner    -> Seer           ->  Ritualist
+Level 0      |  Level 5     |    Level 10    |   Level 15    | Level 20 |   Level 25   |     Level 30
+------------------------------------------------------------------------------------------------------------
+Infanterist -> Footman     ->  Shieldbearer -> Knight       -> Warmaster    ->  Templar    -> Paladin
+Mugger      -> Thief       ->  Rogue        -> Bandit       -> Chunin       -> Renegade    -> Assassin
+Juggler     -> Witcher     -> Enchanter     -> Mage         -> Warlock      -> Dark Caster -> White Sorcerer
+Novice      ->  Proficient ->  Artisan      -> Master       -> Champion     ->  Vindicator -> Paragon
+Caretaker   ->  Tamer      -> Trainer       -> Bowman       -> Hunter       ->  Warden     -> Ranger
+Adventurer  -> Swordsman   ->  Fighter      -> Swashbuckler -> Dragonslayer ->  Raider     -> Eternal Hero
+Priest      ->  Mysticist  -> Doomsayer     -> Seer         -> Oracle       ->  Prophet    -> Ritualist
 ```"""
         )
 
