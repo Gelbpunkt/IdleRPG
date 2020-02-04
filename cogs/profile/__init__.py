e1342d2f92c72f2486232831caaba067c487b143
Jens Reidel <jens@troet.org>
2020-02-04 09:54:35 -0800

    fix some equip logic

--- cogs/profile/__init__.py
+++ cogs/profile/__init__.py
@@ -581,14 +581,12 @@ IdleRPG is a global bot, your characters are valid everywhere"""
                     put_off = all_ids
                 else:
                     if (
-                        (item["hand"] == "left"
-                        or item["hand"] == "right")
-                        and num_any < 2
-                    ):
+                        item["hand"] == "left" or item["hand"] == "right"
+                    ) and num_any < 2:
                         item_to_remove = [
                             i for i in olditems if i["hand"] == item["hand"]
                         ]
-                        if not item_to_remove:
+                        if not item_to_remove and len(olditems) == 2:
                             item_to_remove = [i for i in olditems if i["hand"] == "any"]
                         item_to_remove = item_to_remove[0]["id"]
                         await conn.execute(
@@ -596,6 +594,8 @@ IdleRPG is a global bot, your characters are valid everywhere"""
                             item_to_remove,
                         )
                         put_off = [item_to_remove]
+                    elif item["hand"] == "any" and len(olditems) < 2:
+                        pass  # just so last won't trigger
                     else:
                         item_to_remove = await self.bot.paginator.Choose(
                             title=_("Select an item to unequip"),
