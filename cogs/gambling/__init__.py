5de6d511b96ef9fb06604beb3e5fa2f2e1f9cf8c
Jens Reidel <jens@troet.org>
2020-03-25 05:41:34 -0700

    does not work

--- cogs/gambling/__init__.py
+++ cogs/gambling/__init__.py
@@ -438,7 +438,7 @@ Complicated bets:
     - cheval (number 1) (number 2) (a simple bet on two numbers)
     - plein (number) (a simple bet on one number)
 
-To visualize the rows and columns, use {prefix}roulette table""").format(prefix=ctx.prefix)
+To visualize the rows and columns, use the command: roulette table""")
         if ctx.character_data["money"] < money:
             return await ctx.send(_("You're too poor."))
         try:
