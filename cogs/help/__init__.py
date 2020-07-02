87866964778635370acf975bef6f6446f26a3779
Maximilian Heymann <maxhey4@gmail.com>
2020-07-02 21:44:53 +0200

    revert command.can_run(), use same logic as in cog help

--- cogs/help/__init__.py
+++ cogs/help/__init__.py
@@ -567,8 +567,19 @@ class IdleHelp(commands.HelpCommand):
 
     async def send_command_help(self, command):
         if not await command.can_run(self.context):
-            # this also handles disabled commands
-            return await self.context.send(_("You do not have access to this command!"))
+            if command.cog:
+                if (command.cog.qualified_name in self.gm_exts) and (
+                        self.context.author.id not in self.context.bot.config.game_masters
+                ):
+                    return await self.context.send(
+                        _("You do not have access to this command!")
+                    )
+                if (command.cog.qualified_name in self.owner_exts) and (
+                        self.context.author.id not in self.context.bot.owner_ids
+                ):
+                    return await self.context.send(
+                        _("You do not have access to this command!")
+                    )
 
         e = self.embedbase(
             title=(
