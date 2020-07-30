c8b2581be51fb16bd079a635c873b18747ba8a50
Jens Reidel <jens@troet.org>
2020-07-30 22:00:23 +0200

    Fix chglog config

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -2,7 +2,7 @@
 # add git-chglog to path before
 git-chglog --next-tag $1 -o NEXT_CHANGELOG.md $1
 read -p "Done, please now hit enter, modify it and then I will add it to the changelog file"
-vim NEXT_CHANGELOG.md
+nvim NEXT_CHANGELOG.md
 cat <(cat NEXT_CHANGELOG.md) CHANGELOG.md > CHANGELOG.tmp
 rm CHANGELOG.md NEXT_CHANGELOG.md
 mv CHANGELOG.tmp CHANGELOG.md
