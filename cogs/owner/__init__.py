349b0f913ccfd0798d74fcc2772d248d028db620
Max <maxhey4@gmail.com>
2020-08-03 14:06:29 +0000

    wipe cache for godless playeds

--- cogs/owner/__init__.py
+++ cogs/owner/__init__.py
@@ -153,7 +153,10 @@ class Owner(commands.Cog):
                 text_collection.append(f"{god} set to {luck}.")
             await conn.execute('UPDATE profile SET "favor"=0 WHERE "god" IS NOT NULL;')
             text_collection.append("Godless set to 1.0")
-            await conn.execute('UPDATE profile SET "luck"=1.0 WHERE "god" IS NULL;')
+            ids = await conn.fetch
+                'UPDATE profile SET "luck"=1.0 WHERE "god" IS NULL RETURNING "user";'
+            )
+            all_ids.extend([u["user"] for u in ids])
         await self.bot.cache.wipe_profile(*all_ids)
         msg = await ctx.send("\n".join(text_collection))
         try:
