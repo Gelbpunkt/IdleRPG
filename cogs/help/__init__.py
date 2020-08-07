1c88fe9363a7c8a4ed747bab7341dc6ac39cd78c
Max <maxhey4@gmail.com>
2020-08-07 21:13:17 +0000

    inform the support team when a helpme request was deleted

--- cogs/help/__init__.py
+++ cogs/help/__init__.py
@@ -380,6 +380,10 @@ class Help(commands.Cog):
             return await ctx.send(_("Cancelled cancellation."))
         await self.bot.http.delete_message(self.bot.config.helpme_channel, ctx.helpme)
         await self.bot.redis.execute("DEL", f"helpme:{ctx.guild.id}")
+        message = await self.bot.http.send_message(
+            self.bot.config.helpme_channel, 
+            f"Helpme request for server {ctx.guild} (ctx.guild.id) was cancelled by {ctx.author}"
+        )
         await ctx.send(_("Your helpme request has been cancelled."))
 
     @has_open_help_request()
