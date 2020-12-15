3c632af67fe88cc89f8eb484c4e24a469284809a
Mary Johanna <9-Mary@users.noreply.git.travitia.xyz>
2020-12-15 16:48:05 +0000

    Addition of a requested custom command from our donator

--- cogs/custom/__init__.py
+++ cogs/custom/__init__.py
@@ -16,26 +16,74 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from discord.ext import commands
+from discord import Embed
 
+from classes.converters import MemberConverter
 from utils.i18n import _, locale_doc
+
+
+def is_exzyond():
+    def predicate(ctx):
+        return ctx.author.id == 422954629849022475
+
+    return commands.check(predicate)
+
+
+exz_emotions = {
+    "hug": (
+        "hugs",
+        "https://media.tenor.com/images/d5c635dcb613a9732cfd997b6a048f80/tenor.gif",
+    ),
+    "cuddle": (
+        "cuddles",
+        "https://media.tenor.com/images/735ba60b011d4a1949c902167a1898f9/tenor.gif",
+    ),
+    "kiss": (
+        "kisses",
+        "https://media1.tenor.com/images/af1216d35f8ec076b593401b19ddd0bf/tenor.gif",
+    ),
+    "pat": (
+        "pats",
+        "https://media.tenor.com/images/ed6688a51ea92a8c1de0d58e0e4dcb86/tenor.gif",
+    ),
+    "slap": (
+        "slaps",
+        "https://media.tenor.com/images/f12ea58221df986b9d62eb1a95ee96e1/tenor.gif",
+    ),
+    "punch": (
+        "punches",
+        "https://media.tenor.com/images/df65530317816545e3d3754e647597ed/tenor.gif",
+    ),
+}
 
 
 class Custom(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @is_exzyond()
     @commands.command(
-        hidden=True, brief=_("In loving memory of Mary Johanna, ex-developer.")
+        hidden=True,
+        brief=_("A custom command for ExZyond to express several emotions."),
+        name="ExZ",
     )
     @locale_doc
-    async def onetruechild(self, ctx):
+    async def _exz(self, ctx, user: MemberConverter, emotion: str):
         _(
-            """*In loving memory of Mary Johanna, ex-developer.*
+            """*A custom command for ExZyond to express several emotions.*
 
-            This custom command was added on May 15th 2019 and references the name of the child (ex-)developer Mary Johanna had with User Terror.
-            Anyone can use this command."""
+            This custom group of commands were added on December 1st 2020, requested by User ExZyond."""
         )
-        await ctx.send(_("His name is `#no homo` :heart:"))
+        if (picked_emotion := exz_emotions.get(emotion.lower())):
+            await ctx.send(
+                embed=Embed(
+                    title=f"{ctx.author.display_name} {picked_emotion[0]} {user.display_name}."
+                ).set_image(url=picked_emotion[1])
+            )
+        else:
+            await ctx.send(
+                f"You need to provide one from the following: `{', '.join(exz_emotions.keys())}`!"
+            )
 
 
 def setup(bot):
